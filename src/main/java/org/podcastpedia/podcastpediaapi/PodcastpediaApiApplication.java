c87c64b0a182737e2dd8e2a1c8e6a245712de3a2
Adrian Matei <ama@codingpedia.org>
2018-11-15 06:52:47 +0100

    First commit

---
+++ src/main/java/org/podcastpedia/podcastpediaapi/PodcastpediaApiApplication.java
@@ -0,0 +1,12 @@
+package org.podcastpedia.podcastpediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastpediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastpediaApiApplication.class, args);
+	}
+}
